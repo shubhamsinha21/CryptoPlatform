fa6e54f5d98b609ccc6df2a7d25dcfbfa8de1b39
rozer <shubhamsinha07084@gmail.com>
2024-08-04 14:54:41 +0530

    added cutom data and images

---
+++ CustomData/index.js
@@ -0,0 +1,72 @@
+export const topPublisher = [
+  {
+    name: "Business Insider",
+    url: "https://www.businessinsider.com/investing-strategy-financial-independence-millionaire-wall-street-trader-tiktok-influencer-2022-2?IR=T",
+    img: "/business.svg",
+  },
+  {
+    name: "Science Direct",
+    url: "https://www.sciencedirect.com/science/article/abs/pii/S1544612323006712",
+    img: "/science.svg",
+  },
+  {
+    name: "CNBC",
+    url: "https://www.cnbc.com/2020/11/24/square-and-paypal-emerge-as-whales-in-the-crypto-market-.html",
+    img: "/cnbc.svg",
+  },
+  {
+    name: "Fortune",
+    url: "https://fortune.com/crypto/2023/05/25/nft-marketplace-magic-eden-will-pay-you-to-buy-and-sell-solana-nfts/",
+    img: "/fortune.webp",
+  },
+  {
+    name: "CoinDesk",
+    url: "https://www.coindesk.com/markets/2014/07/01/holytransaction-bets-big-on-universal-cryptocurrency-wallet/",
+    img: "/coin-desk.svg",
+  },
+  {
+    name: "Decrypt",
+    url: "",
+    img: "/decrypt.webp",
+  },
+  {
+    name: "Forbes",
+    url: "https://www.forbes.com/sites/michaeldelcastillo/2020/12/02/visa-partners-with-ethereum-digital-dollar-startup-that-raised-271-million/?sh=6b6547714b1f",
+    img: "/forbes.webp",
+  },
+  {
+    name: "Business Insider",
+    url: "https://www.businessinsider.com/investing-strategy-financial-independence-millionaire-wall-street-trader-tiktok-influencer-2022-2?IR=T",
+    img: "/business.svg",
+  },
+  {
+    name: "Science Direct",
+    url: "https://www.sciencedirect.com/science/article/abs/pii/S1544612323006712",
+    img: "/science.svg",
+  },
+  {
+    name: "CNBC",
+    url: "https://www.cnbc.com/2020/11/24/square-and-paypal-emerge-as-whales-in-the-crypto-market-.html",
+    img: "/cnbc.svg",
+  },
+  {
+    name: "Fortune",
+    url: "https://fortune.com/crypto/2023/05/25/nft-marketplace-magic-eden-will-pay-you-to-buy-and-sell-solana-nfts/",
+    img: "/fortune.webp",
+  },
+  {
+    name: "CoinDesk",
+    url: "https://www.coindesk.com/markets/2014/07/01/holytransaction-bets-big-on-universal-cryptocurrency-wallet/",
+    img: "/coin-desk.svg",
+  },
+  {
+    name: "Decrypt",
+    url: "",
+    img: "/decrypt.webp",
+  },
+  {
+    name: "Forbes",
+    url: "https://www.forbes.com/sites/michaeldelcastillo/2020/12/02/visa-partners-with-ethereum-digital-dollar-startup-that-raised-271-million/?sh=6b6547714b1f",
+    img: "/forbes.webp",
+  },
+];
